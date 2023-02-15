648ecb456ebd3c05fe1b68a10b2d02cf9b271dd9
Peter Zhu <peter@peterzhu.ca>
2023-02-15 16:06:17 -0500

    Don't install Ruby in setup.sh

--- setup.sh
+++ setup.sh
@@ -23,23 +23,3 @@ sudo apt install -y htop psmisc
 # Dependencies for MRI development
 sudo apt install -y git ruby autoconf bison gcc make zlib1g-dev libffi-dev libreadline-dev libgdbm-dev libssl-dev
 sudo apt install -y lldb gdb
-
-# chruby
-wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
-tar -xzvf chruby-0.3.9.tar.gz
-cd chruby-0.3.9/
-sudo make install
-cd ..
-rm -rf chruby-0.3.9.tar.gz chruby-0.3.9
-
-# ruby-install
-wget -O ruby-install-0.8.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.8.1.tar.gz
-tar -xzvf ruby-install-0.8.1.tar.gz
-cd ruby-install-0.8.1/
-sudo make install
-cd ..
-rm -rf ruby-install-0.8.1.tar.gz ruby-install-0.8.1
-
-# Ruby 3.0.2
-ruby-install ruby 3.0.2
-
